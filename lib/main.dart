5e311f53fab4bab8e5a99f25d1f618b1d5e69e57
Abdelrahman6481 <cnario.bedo@gmail.com>
2025-03-15 01:40:38 +0200

    update main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'pages/prelogin.dart';
-import 'package:flutter/services.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const MyApp());
 }
 
