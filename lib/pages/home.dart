4bca32bed97679846e89dfde18d217d2b14f800c
Abdelrahman6481 <cnario.bedo@gmail.com>
2025-03-17 15:00:41 +0200

    create navbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    HomeContent(),
+    Center(child: Text('Schedule Page', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Restrictions Page', style: TextStyle(fontSize: 24))),
+    Center(child: Text('Profile Page', style: TextStyle(fontSize: 24))),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +50,7 @@ class HomePage extends StatelessWidget {
                     radius: 40,
                     backgroundImage: AssetImage('assets/profile.png'),
                   ),
-                  SizedBox(height: 30),
+                  SizedBox(height: 20),
                   Text('Abdelrahman Elasaeed', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold)),
                   Text('202106409', style: TextStyle(color: Colors.white70)),
                 ],
@@ -49,86 +65,116 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.red.shade900,
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage: AssetImage('assets/profile.png'),
-                  ),
-                  SizedBox(width: 15),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Hi - Abdelrahman Elasaeed',
-                        style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      Text('202106409', style: TextStyle(color: Colors.white, fontSize: 16)),
-                      Text('Level 4', style: TextStyle(color: Colors.white70, fontSize: 16)),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Text('Announcements', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            SizedBox(height: 10),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              padding: EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  Text(
-                    'الهندسة المعمارية يرجى العلم بضرورة اجتياز امتحان القبول...',
-                    style: TextStyle(fontSize: 14),
-                    textAlign: TextAlign.right,
-                  ),
-                  SizedBox(height: 10),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text("View All", style: TextStyle(color: Colors.red.shade900)),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            SizedBox(
-              height: 120,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  _buildDashboardItem(Icons.calendar_today, 'Schedule'),
-                  _buildDashboardItem(Icons.check_circle, 'Attendance'),
-                  _buildDashboardItem(Icons.assignment, 'Assignments'),
-                  _buildDashboardItem(Icons.grade, 'Results'),
-                  _buildDashboardItem(Icons.payment, 'Fees'),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        color: Colors.red.shade900,
+        buttonBackgroundColor: Colors.redAccent,
+        height: 60,
+        animationDuration: Duration(milliseconds: 300),
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          Icon(Icons.home, size: 30, color: Colors.white),
+          Icon(Icons.access_time, size: 30, color: Colors.white),
+          Icon(Icons.block, size: 30, color: Colors.white),
+          Icon(Icons.person, size: 30, color: Colors.white),
+        ],
+        index: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buildDrawerItem(IconData icon, String title, BuildContext context) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.black),
+      title: Text(title),
+      onTap: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.red.shade900,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage('assets/cic_logo2.png'),
+                ),
+                SizedBox(width: 15),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Hi - Abdelrahman ',
+                      style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text('202106409', style: TextStyle(color: Colors.white, fontSize: 16)),
+                    Text('Level 4', style: TextStyle(color: Colors.white70, fontSize: 16)),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          Text('Announcements', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          SizedBox(height: 10),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Text(
+                  'الهندسة المعمارية يرجى العلم بضرورة اجتياز امتحان القبول...',
+                  style: TextStyle(fontSize: 14),
+                  textAlign: TextAlign.right,
+                ),
+                SizedBox(height: 10),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text("View All", style: TextStyle(color: Colors.red.shade900)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          SizedBox(
+            height: 120,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                _buildDashboardItem(Icons.calendar_today, 'Schedule'),
+                _buildDashboardItem(Icons.check_circle, 'Attendance'),
+                _buildDashboardItem(Icons.assignment, 'Assignments'),
+                _buildDashboardItem(Icons.grade, 'Results'),
+                _buildDashboardItem(Icons.payment, 'Fees'),
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -157,16 +203,6 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-
-  Widget _buildDrawerItem(IconData icon, String title, BuildContext context) {
-    return ListTile(
-      leading: Icon(icon, color: Colors.black),
-      title: Text(title),
-      onTap: () {
-        Navigator.pop(context);
-      },
     );
   }
 }
